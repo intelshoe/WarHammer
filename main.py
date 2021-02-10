e4e9721b7b9394a54e4c1a5374a79503d5a15012
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-02-09 23:12:34 -0500

    sleep time added

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Status: under construction.
 '''
 
 import speech_recognition as sr
+import time
 
 # obtain audio from the microphone
 # requires PyAudio (only supports python 3.6)
@@ -15,6 +16,11 @@ with sr.Microphone() as source:
     print("Say something!")
     audio = r.listen(source)
 
+time.sleep(30)
+
 # write audio to a WAV file
 with open("microphone-results.wav", "wb") as f:
     f.write(audio.get_wav_data())
+
+
+quit()
