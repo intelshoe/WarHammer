caca5b7f0612740761e06977b2726def82db3346
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-02-09 23:24:19 -0500

    removes sphinx

--- main.py
+++ main.py
@@ -31,14 +31,6 @@ r = sr.Recognizer()
 with sr.AudioFile(AUDIO_FILE) as source:
     audio = r.record(source)  # read the entire audio file
 
-# recognize speech using Sphinx
-try:
-    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-except sr.UnknownValueError:
-    print("Sphinx could not understand audio")
-except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e))
-
 # recognize speech using Google Speech Recognition
 try:
     # for testing purposes, we're just using the default API key
