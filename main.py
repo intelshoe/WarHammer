f0f908a767cf05baac4ea6d2d56b3a19711492dd
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-02-09 22:37:03 -0500

    voice recog

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ WarHammer - a modern phreaker's war dialing sword.
 Status: under construction.
 '''
 
+import speech_recognition as sr
+
+# obtain audio from the microphone
+# requires PyAudio
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+# write audio to a WAV file
+with open("microphone-results.wav", "wb") as f:
+    f.write(audio.get_wav_data())
