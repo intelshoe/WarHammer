044e0aee36ee65cd755b87163ec703cafe345220
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-02-14 01:53:58 -0500

    mic class works!

--- main.py
+++ main.py
@@ -1,53 +1,19 @@
 #!/usr/bin/env python3
-
 '''
-This section records audio
+Main app entry point.
 
-Needs work:
-break parts into class
 '''
 import speech_recognition as sr
 import time
 from os import path
-
-def speachToTxt():
-    # obtain audio from the microphone
-    # requires PyAudio (only supports python 3.6)
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Say something!")
-        audio = r.listen(source)
-
-    time.sleep(10)
-
-    #  making a change to show you
-    #making a change!!!
-
-    # write audio to a WAV file
-    with open("microphone-results.wav", "wb") as f:
-        f.write(audio.get_wav_data())
-
-    # begin code to transcribe audio to text
-    AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "microphone-results.wav")
-
-    # use the audio file as the audio source
-    r = sr.Recognizer()
-    with sr.AudioFile(AUDIO_FILE) as source:
-        audio = r.record(source)  # read the entire audio file
-
-    # recognize speech using Google Speech Recognition
-    try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        print("I think I heard: " + r.recognize_google(audio))
-    except sr.UnknownValueError:
-        print("Could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
-    return
+from talk import Talk
 
 def main():
-    speachToTxt()
+    # start the talking feature
+    talkBot = Talk()
 
+    # record 10 second to wav file
+    talkBot.record(10)
+
+# runs the whole program
 main()
--- talk.py
+++ talk.py
@@ -1,4 +1,4 @@
-''''
+'''
 Main interface to features.
 
 Talk.record(secondsToRecord)
@@ -7,6 +7,25 @@ Talk.type(secondsToRecord | recording)
 
 Talk.play(soundToPlay)
 
+'''
+import speech_recognition as sr
+import time
+from os import path
 
+class Talk():
+    def __init__(self):
+        seconds = 0
 
-''''
+    def __str__():
+        return "Talk Options"
+
+    # saves audio to wav file
+    def record(self, seconds):
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Say something!")
+            audio = r.listen(source)
+        time.sleep(seconds)
+        # write audio to a WAV file
+        with open("microphone-results.wav", "wb") as f:
+            f.write(audio.get_wav_data())
