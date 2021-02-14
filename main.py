36e20428de63e13e932955bad02e6d806a346046
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-02-13 21:40:35 -0500

    made speachToTxt func

--- main.py
+++ main.py
@@ -10,36 +10,41 @@ import speech_recognition as sr
 import time
 from os import path
 
-# obtain audio from the microphone
-# requires PyAudio (only supports python 3.6)
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+def speachToTxt():
+    # obtain audio from the microphone
+    # requires PyAudio (only supports python 3.6)
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
 
-time.sleep(10)
+    time.sleep(10)
 
-# write audio to a WAV file
-with open("microphone-results.wav", "wb") as f:
-    f.write(audio.get_wav_data())
+    # write audio to a WAV file
+    with open("microphone-results.wav", "wb") as f:
+        f.write(audio.get_wav_data())
 
-# begin code to transcribe audio to text
-AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "microphone-results.wav")
+    # begin code to transcribe audio to text
+    AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "microphone-results.wav")
 
-# use the audio file as the audio source
-r = sr.Recognizer()
-with sr.AudioFile(AUDIO_FILE) as source:
-    audio = r.record(source)  # read the entire audio file
+    # use the audio file as the audio source
+    r = sr.Recognizer()
+    with sr.AudioFile(AUDIO_FILE) as source:
+        audio = r.record(source)  # read the entire audio file
 
-# recognize speech using Google Speech Recognition
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    # recognize speech using Google Speech Recognition
+    try:
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
+        print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    return
 
-quit()
+def main():
+    speachToTxt()
+
+main()
